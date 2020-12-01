c42b203b391d99a5fcc42bd56f60b8125c6d95ec
Atharva Kulkarni <atharvakulkarni2204@gmail.com>
2020-12-01 22:29:02 +0530

    Initial commit

---
+++ app/src/main/java/dev/atharvakulkarni/futurefurnitureui/Main2Activity.java
@@ -0,0 +1,58 @@
+package dev.atharvakulkarni.futurefurnitureui;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class Main2Activity extends AppCompatActivity
+{
+    LinearLayout personLinearLayout;
+    FloatingActionButton favorite;
+    CardView cactusCardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        personLinearLayout = findViewById(R.id.person_linear_layout);
+        favorite = findViewById(R.id.favorite);
+        cactusCardView = findViewById(R.id.cactus_card_view);
+        personLinearLayout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(),Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+        favorite.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(),Main4Activity.class);
+                startActivity(intent);
+            }
+        });
+        cactusCardView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(),Main5Activity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/dev/atharvakulkarni/futurefurnitureui/Main4Activity.java
@@ -0,0 +1,19 @@
+package dev.atharvakulkarni.futurefurnitureui;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main4Activity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+}
